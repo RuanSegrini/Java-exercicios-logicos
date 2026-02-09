e66a8b0b279fde33a4775e93882e7562356474e6
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-08 23:21:55 -0300

    Create Exercicio53.java

---
+++ 01-sequencial/Exercicio53.java
@@ -0,0 +1,26 @@
+package sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio53 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o comprimento do terreno: ");
+        double comprimento = sc.nextDouble();
+        System.out.print("Digite a largura do terreno: ");
+        double largura = sc.nextDouble();
+        System.out.print("Digite o valor de metro quadrado: ");
+        double metroQuadradoValor = sc.nextDouble();
+
+        double custo = largura * comprimento * metroQuadradoValor;
+
+        System.out.printf("Custo para cercar o terreno: R$%.2f", custo);
+        
+        sc.close();
+    }
+}
