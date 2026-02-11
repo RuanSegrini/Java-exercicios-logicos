a0bb19b51f73538c5bd38bae0ffe90c166c0eeca
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-10 23:55:19 -0300

    Update Exercicio01.java

--- 02-condicionais/Exercicio01.java
+++ 02-condicionais/Exercicio01.java
@@ -1,1 +1,26 @@
+package condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[]args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite dois numeros: ");
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if(x > y){
+            System.out.printf("Maior: %.2f", x);
+        }
+        else{
+            System.out.printf("Maior: %.2f", y);
+        }
+
+        sc.close();
+    }
+}
