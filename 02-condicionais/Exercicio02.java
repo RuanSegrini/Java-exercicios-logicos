ff2079add41214d21560289ef1f4f5e07cc7acab
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-11 00:21:19 -0300

    Update Exercicio02.java

--- 02-condicionais/Exercicio02.java
+++ 02-condicionais/Exercicio02.java
@@ -1,1 +1,26 @@
+package condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[]args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        double N = sc.nextDouble();
+
+        if(N >= 0){
+            double raizQuadrada = Math.sqrt(N);
+            System.out.printf("Raiz quadrada: %.2f", raizQuadrada);
+        }
+        else{
+            System.out.print("Número invalido");
+        }
+
+        sc.close();
+    }
+}
