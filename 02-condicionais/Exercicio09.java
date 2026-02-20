aaad2a9eec6c2f0e83045c11680a037ca05381dd
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-19 23:36:18 -0300

    Implement loan approval based on salary percentage

--- 02-condicionais/Exercicio09.java
+++ 02-condicionais/Exercicio09.java
@@ -1,1 +1,29 @@
+package condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio09 {
+
+    public static void main(String[]args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o salario do trabalhador: ");
+        double salario = sc.nextDouble();
+        System.out.print("Informe o valor da prestaçao de um emprestimo: ");
+        double prestacao = sc.nextDouble();
+
+        double calcularPorcentagem = 0.20 * salario;
+
+        if(prestacao > calcularPorcentagem){
+            System.out.print("EMPRESTIMO NAO CONCEDIDO! ");
+        }
+        else{
+            System.out.print("EMPRESTIMO CONCEDIDO! ");
+        }
+
+        sc.close();
+    }
+}
