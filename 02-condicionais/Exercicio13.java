3f9f0d0138ccd6f8fb4eff177b8fd57300a0b5b6
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-23 16:04:39 -0300

    Add Exercicio13 for calculating student grades

---
+++ 02-condicionais/Exercicio13.java
@@ -0,0 +1,32 @@
+package condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio13 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a nota das 3 provas do aluno: ");
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+        double nota3 = sc.nextDouble();
+
+        double notaTotal = nota1 + nota2 + (nota3 * 2);
+        double media = notaTotal / 3;
+
+        System.out.printf("Media: %.2f%n", media);
+        
+        if(notaTotal >= 60.0){
+            System.out.print("APROVADO! ");
+        }
+        else{
+            System.out.print("REPROVADO! ");
+        }
+
+        sc.close();
+    }
+}
