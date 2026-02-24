7975ffb66b75101fef8e3c8fa2efb9c2814cda73
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-24 17:54:36 -0300

    Add program to display day of the week by number

--- 02-condicionais/Exercicio15.java
+++ 02-condicionais/Exercicio15.java
@@ -1,1 +1,46 @@
+package condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+    public static void main(String[]args){
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Informe um numero entre 1 e 7 para saber o dia da semana correspondente: ");
+        int dia = sc.nextInt();
+
+        switch(dia){
+            case 1:
+                System.out.print("Domingo");
+                break;
+            case 2:
+                System.out.print("Segunda-Feira");
+                break;
+            case 3:
+                System.out.print("Terça-Feira");
+                break;
+            case 4:
+                System.out.print("Quarta-Feira");
+                break;
+            case 5:
+                System.out.print("Quinta-Feira");
+                break;
+            case 6:
+                System.out.print("Sexta-Feira");
+                break;
+            case 7:
+                System.out.print("Sabado");
+                break;
+            default:
+                System.out.print("Numero invalido! Digite um valor entre 1 e 7.");
+        }
+
+        sc.close();
+    }
+}
