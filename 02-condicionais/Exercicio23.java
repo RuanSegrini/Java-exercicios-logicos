c3502e6a0714818bd93fe8ed831e617d1d9a807c
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2026-02-27 23:55:17 -0300

    Add leap year check in Exercicio23.java

--- 02-condicionais/Exercicio23.java
+++ 02-condicionais/Exercicio23.java
@@ -1,1 +1,26 @@
+package condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio23 {
+
+    public static void main(String[]args){
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um ano para saber se ele é bissexto: ");
+        int ano = sc.nextInt();
+
+        if(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0){
+            System.out.print("ANO BISSEXTO!");
+        }
+        else{
+            System.out.print("ANO NÃO BISSEXTO! ");
+        }
+
+        sc.close();
+    }
+}
